8835cf8e2ca3893ff3f65ff282b2b71fccf6d81b
Marcelo-Alves-Junior <marcelo.cont@live.com>
2024-12-01 17:09:35 -0300

    commit

--- aula1.py
+++ aula1.py
@@ -7,30 +7,56 @@
 
 import pyautogui
 import time
+import pandas
 
 pyautogui.PAUSE = 1
 link = "https://dlp.hashtagtreinamentos.com/python/intensivao/login"
 
-#abrir o chrome
+# Passo 1: Entrar no sistema da empresa 
+    # https://dlp.hashtagtreinamentos.com/python/intensivao/login
+
 pyautogui.press("win")
 pyautogui.write("chrome")
 pyautogui.press("enter")
 
+# Passo 2: Fazer Login
 pyautogui.write(link)
 pyautogui.press("enter")
 time.sleep(3)
 pyautogui.click(x=768, y=371)
 pyautogui.write("teste@teste.com")
-pyautogui.click(x=768, y=371)
+pyautogui.press("tab")
 pyautogui.write("teste_senha")
-
-pyautogui.write("chrome")
-pyautogui.press("enter")
-
-pyautogui.write(link)
+pyautogui.press("tab")
 pyautogui.press("enter")
 time.sleep(3)
-pyautogui.click(x=768, y=371)
-pyautogui.write("teste@teste.com")
-pyautogui.click(x=768, y=371)
-pyautogui.write("teste_senha")
+
+# Passo 3: Importar a base de dados do Produto
+tabela = pandas.read_csv("C:/Users/Dev/Desktop/Projetos Pessoais/Python Hashtag/Curso-de-Python/produtos.csv")
+print(tabela) 
+
+# Passo 4: Cadastrar 1 produto
+def cadastrar_produto (linha):
+    pyautogui.write(str(tabela.loc[linha, "codigo"]))
+    pyautogui.press("tab")
+    pyautogui.write(str(tabela.loc[linha, "marca"]))
+    pyautogui.press("tab")
+    pyautogui.write(str(tabela.loc[linha, "tipo"]))
+    pyautogui.press("tab")
+    pyautogui.write(str(tabela.loc[linha, "categoria"]))
+    pyautogui.press("tab")
+    pyautogui.write(str(tabela.loc[linha, "preco_unitario"]))
+    pyautogui.press("tab")
+    pyautogui.write(str(tabela.loc[linha, "custo"]))
+    pyautogui.press("tab")
+    pyautogui.write(str(tabela.loc[linha, "obs"]))
+    pyautogui.press("tab")
+    pyautogui.press("enter")
+    time.sleep(3)
+
+# Passo 5: Repetir cadastro para todos os produtos
+
+for linha in tabela.index:
+    pyautogui.click(x=880, y=257)
+    cadastrar_produto(linha)
+    pyautogui.scroll(1000)
---
+++ dicas.py
@@ -0,0 +1,19 @@
+#Dicas para automatizar
+
+# Biblioteca para aguardar carregamento da tela
+import time
+
+time.sleep(3)
+
+# Biblioteca para pegar posição do mouse (Utilizar o time.sleep para posicionar o mouse aonde se quer pegar a posição)
+# Demais funções acessar: https://pyautogui.readthedocs.io/en/latest/index.html
+import pyautogui
+
+print(pyautogui.position())
+
+# Biblioteca para importar arquivos excel, csv e etc
+
+import pandas
+
+tabela = pandas.read_csv("C:/Users/Dev/Desktop/Projetos Pessoais/Python Hashtag/Curso-de-Python/produtos.csv")
+print(tabela)
